ee3febda626c39e6f4dfb0143f0040e20c0740b1
maoyuwang <i@maoyu.wang>
2025-08-26 15:29:13 -0400

    init

---
+++ src/main/java/com/insightfinder/gateway/router/ChatCompletionsRouter.java
@@ -0,0 +1,10 @@
+package com.insightfinder.gateway.router;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/chat/completions")
+public class ChatCompletionsRouter {
+
+}
